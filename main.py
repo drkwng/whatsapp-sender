f391ff0f7420ebf18320f2d3d2a8f3b82bd26b89
drkwng <iamdrkwng@gmail.com>
2022-03-06 19:53:15 +0200

    WhatsApp Sender

---
+++ main.py
@@ -0,0 +1,30 @@
+# pip install -r requirements.txt
+
+from whatsapp import WhatsAppSender
+
+
+def file_reader(filename):
+    data = []
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f.read().splitlines():
+            data.append(line)
+    return data
+
+
+if __name__ == "__main__":
+    print("Start. Слава Україні!")
+
+    numbers_file = 'numbers.txt'
+    message_file = 'message.txt'
+
+    numbers = file_reader(numbers_file)
+    message = ' '.join(file_reader(message_file))
+
+    if len(numbers) < 1:
+        print('Add phone numbers to numbers.txt')
+    elif len(message) < 1:
+        print('Add your message to message.txt')
+    else:
+        whatsapp = WhatsAppSender()
+        whatsapp.worker(numbers, message)
+        print('Done. Героям слава!')
---
+++ whatsapp.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as condition
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+from time import sleep
+import os
+
+
+class WhatsAppSender:
+    def __init__(self):
+        os.environ["WDM_LOG_LEVEL"] = "0"
+
+        options = Options()
+        options.add_experimental_option("excludeSwitches", ["enable-logging"])
+        options.add_argument("--profile-directory=Default")
+        options.add_argument("--user-data-dir=/var/tmp/chrome_user_data")
+        self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+    def send_message(self, number, message):
+        url = f'https://web.whatsapp.com/send?phone={number}&text={message}'
+        self.driver.get(url)
+        try:
+            button = WebDriverWait(self.driver, 15).until(condition.element_to_be_clickable((
+                By.XPATH, "//button[@class='_4sWnG']"
+            )))
+            sleep(1)
+            button.click()
+            print(f'{number} - Message has sent')
+
+        except Exception as err:
+            print(f'{number} - Sending error. '
+                  f'No connection to the Internet or the number is not valid')
+
+            input("Make sure that the device and computer are"
+                  "connected to the Internet and press Enter...")
+
+    def worker(self, number_list, message):
+        self.driver.get('https://web.whatsapp.com')
+        input("After authorizing on WhatsApp press Enter...")
+        for number in number_list:
+            if len(number) > 8:
+                print(f"{number} - Start sending")
+                self.send_message(number, message)
+                sleep(3)
+
+        self.driver.close()
+
+
+if __name__ == "__main__":
+
+    nums = ['+380670000000', ]
+    msg = 'test'
+
+    whatsapp = WhatsAppSender()
+    whatsapp.worker(nums, msg)
+
+
+
+
